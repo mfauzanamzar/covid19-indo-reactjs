49362f3784007ad42a2dca99c3233d33c7bb1cbe
mfauzanamzar <mfauzanamzar@gmail.com>
2021-07-13 05:47:57 +0800

    add share button

--- src/components/Provinsi.js
+++ src/components/Provinsi.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../index.css';
 import axios from 'axios';
+import { FacebookIcon, WhatsappIcon, WhatsappShareButton } from 'react-share';
 
 
 const Provinsi = () => {
@@ -10,7 +11,7 @@ const Provinsi = () => {
     useEffect(() => {
         axios.get('https://apicovid19indonesia-v2.vercel.app/api/indonesia/provinsi')
             .then(res => {
-              
+
                 setData(res.data)
             })
             .catch(err => {
@@ -20,38 +21,52 @@ const Provinsi = () => {
 
 
     return (
-      <div className="container">
-          <div className="provinsi">
-          <h1>Data Provinsi</h1>
-                    
-            <table className="tabel">
-                
-                <tr>
-                   <th className="tbl-head">Nama Provinsi</th>
-                   <th className="tbl-head">Positif</th>
-                   <th className="tbl-head">Meninggal</th>
-                   <th className="tbl-head">Sembuh</th>
-                   <th className="tbl-head">Dirawat</th>
-                </tr>
-                
+        <div className="container">
+            <div className="provinsi">
+                <div className="provHeader">
+                    <h1>Data Provinsi</h1>
+                    <div className="share">
+                        <div className="text"><a>Share : </a></div>
+                        <div className="wa">
+                            <WhatsappShareButton
+                                url="https://indonesia-covid-19.netlify.app/"
+                                title="Jumlah Covid-19 Indonesia" 
+                               >
+                                <WhatsappIcon round={true} className="wa-icon" />
+                            </WhatsappShareButton>
+                        </div>
+                        <div className="fb"><FacebookIcon round={true} className="fb-icon" /></div>
 
-                {data.map(dt=>(
-                    
-                <tr className="tbl-prov" key={dt.provinsi}>
-                    <td className="nmprov">{dt.provinsi}</td>
-                    <td>{dt.kasus}</td>
-                    <td>{dt.meninggal}</td>
-                    <td>{dt.sembuh}</td>
-                    <td>{dt.dirawat}</td>
-                </tr>
-            
-                    ))}         
-            </table>
-             
-          </div>
+                    </div>
+                </div>
+                <table className="tabel">
+
+                    <tr>
+                        <th className="tbl-head">Nama Provinsi</th>
+                        <th className="tbl-head">Positif</th>
+                        <th className="tbl-head">Meninggal</th>
+                        <th className="tbl-head">Sembuh</th>
+                        <th className="tbl-head">Dirawat</th>
+                    </tr>
 
 
-      </div>
+                    {data.map(dt => (
+
+                        <tr className="tbl-prov" key={dt.provinsi}>
+                            <td className="nmprov">{dt.provinsi}</td>
+                            <td>{dt.kasus}</td>
+                            <td>{dt.meninggal}</td>
+                            <td>{dt.sembuh}</td>
+                            <td>{dt.dirawat}</td>
+                        </tr>
+
+                    ))}
+                </table>
+
+            </div>
+
+
+        </div>
     )
 }
 
